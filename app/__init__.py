4b967babea01cf868b2bf569458394f97b0eb6a1
LuisSoares66 <luissoares.lfs@gmail.com>
2026-02-05 14:12:29 -0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-from app import models  # garante que os models sejam carregados
 
-# ======================================================
-# EXTENSÕES (instanciadas UMA ÚNICA VEZ)
-# ======================================================
 db = SQLAlchemy()
 migrate = Migrate()
 
 
-# ======================================================
-# FACTORY DO APP
-# ======================================================
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -21,9 +14,11 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from app import models  # <<< ADICIONE ISSO
+    # ✅ IMPORTA MODELS AQUI (depois do db existir)
+    from . import models  # noqa: F401
 
-    from app.routes import bp
+    # Blueprints
+    from .routes import bp
     app.register_blueprint(bp)
 
     return app
