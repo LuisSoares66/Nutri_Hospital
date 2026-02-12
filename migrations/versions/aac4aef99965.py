d5039574e6057c94cc2f07a5276ae38dd79d36d3
LuisSoares66 <luissoares.lfs@gmail.com>
2026-02-12 14:32:53 -0300

    Add files via upload

---
+++ migrations/versions/aac4aef99965.py
@@ -0,0 +1,17 @@
+"""base revision aac4aef99965
+
+Arquivo criado para restaurar a cadeia do Alembic no deploy.
+"""
+
+from alembic import op
+
+revision = "aac4aef99965"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
