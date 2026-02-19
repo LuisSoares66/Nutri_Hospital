233c63b906bed2b8dcb70a4c6e096f986f2f589f
LuisSoares66 <luissoares.lfs@gmail.com>
2026-02-19 10:44:22 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "Lincinha")
-
-    # Render fornece DATABASE_URL
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_pre_ping": True,
-        "pool_recycle": 280,
+        "pool_pre_ping": True,        # testa conexão antes de usar
+        "pool_recycle": 280,          # recicla antes do timeout comum
+        "pool_timeout": 30,
+        "pool_size": 5,
+        "max_overflow": 2,
         "connect_args": {"sslmode": "require"},
     }
+
 
     # Login do botão ADMIN
     ADMIN_USER = os.environ.get("ADMIN_USER", "admin")
